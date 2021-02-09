681771f12951268e86392ab39d219e268661b876
Adam <adamchehab@gmail.com>
2021-02-10 01:37:34 +0300

    Fixed all, but bad solution

--- main.c
+++ main.c
@@ -7,10 +7,14 @@ void	ft_init_spec(t_spec **spec)
 	(*spec)->width = 0;
 	(*spec)->accuracy = 0;
 	(*spec)->type = '0';     // char ex: d i u x X
+	(*spec)->valid = 0;
 }
 
-void ft_flags_parser(const char **format, t_spec **spec)
+int		ft_flags_parser(const char **format, t_spec **spec)
 {
+	int len;
+
+	len = 0;
 	while ((ft_strchr("-0 ", **format)))
 	{
 		if ((**format == '-') && ((*spec)->flag != '-'))
@@ -21,33 +25,59 @@ void ft_flags_parser(const char **format, t_spec **spec)
 			(*spec)->space = 1;
 		(*format)++;
 	}
-
+	return (len);
 }
 
-void	ft_parser(const char **format, t_spec *spec)
+int		ft_parser(const char *format, t_spec *spec)
 {
-	(*format)++;         // skip %
+	int len;
+	format++;         // skip %
 	ft_init_spec(&spec); // initialize spec
 
-	ft_flags_parser(format, &spec);
-	//parse width and accuracy
-	if (ft_strchr("diucspxX%", **format))
-		spec->type = **format;
+	len = ft_flags_parser(&format, &spec);
+	// parse width
+	// parse accuracy
+	if (ft_strchr("diucspxX%", *format))
+	{
+		spec->type = *format;
+		return (len + 1);
+	}
+	if ((*(format + 1)) == '\0')
+		return (-1);
+	else
+		spec->valid = 1;
+	return (len + 1); // + 1 cuz %
+}
+
+int		ft_handler(va_list ap, t_spec spec)
+{
+	int len;
+
+	len = 0;
+	if ((spec.type == 'd') || (spec.type == 'i'))
+		len += ft_putnbr_di(ap);
+	else if (spec.type == 1)
+		len += ft_putchar('%');
+	return (len);
 }
 
 int		ft_handler_ap(va_list ap, const char **format)
 {
 	int len;
+	int parse;
 	t_spec spec;
 
-	ft_parser(format, &spec);
-	if ((spec.type == 'd') || (spec.type == 'i'))
-		len += ft_putnbr_di(ap);
-	else if (spec.type == 1)
-		len += ft_putchar('%');
+	len = 0;
+	parse = ft_parser(*format, &spec);
 
-	if (**format != '\0')
-		(*format)++;
+	if (parse == -1)    // error
+		return (-1);
+	if (spec.valid == 1)
+		len += ft_putchar('%') + parse - 1;
+	else if (parse > 0)
+		len += ft_handler(ap, spec);
+	
+	(*format) += parse + 1;
 	return (len);
 }
 
@@ -86,7 +116,7 @@ int		main(void)
 {
 	int i = 0;
 	int j = 0;
-	char test_str[] = "test %d number";
+	char test_str[] = "test %z 123";
 
 	// my
     i = ft_printf(test_str, 1);
