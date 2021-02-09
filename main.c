b99e62f144b04ecb5a53efb9a14c8f3427901621
Adam <adamchehab@gmail.com>
2021-02-09 03:15:21 +0300

    all same but with parser

--- main.c
+++ main.c
@@ -1,38 +1,37 @@
 #include "main.h"
 
-void ft_init_flags(t_flags *flags)
+void ft_init_flags(t_flags **flags)
 {
-	flags->type = '0';     // char ex: d i u x X
-	flags->accuracy = 0;   //number
-	flags->width = 0;      // number
-	flags->percent = 0;    // 1 | 0
-	flags->minus = 0;      // 1 | 0
-	flags->space = 0;      // 1 | 0
-	flags->zero = 0;       // 1 | 0
+	(*flags)->type = '0';     // char ex: d i u x X
+	(*flags)->accuracy = 0;   //number
+	(*flags)->width = 0;      // number
+	(*flags)->percent = 0;    // 1 | 0
+	(*flags)->minus = 0;      // 1 | 0
+	(*flags)->space = 0;      // 1 | 0
+	(*flags)->zero = 0;       // 1 | 0
 }
-
 
 void ft_parser(const char **format, t_flags *flags)
 {
-
+	(*format)++;
+	ft_init_flags(&flags);
+	if ((**format == 'd') || (**format == 'i'))
+		flags->type = 'd';
+	else if (**format == '%')
+		flags->percent = 1;
 }
 
 int ft_handler_ap(va_list ap, const char **format)
 {
 	int len;
-	//t_flags flags
+	t_flags flags;
 
-	len = 0;
-	// ft_parse()...
-	(*format)++;
-	if ((**format == 'd') || (**format == 'i'))
+	ft_parser(format, &flags);
+	if (flags.type == 'd')
 		len += ft_putnbr_di(ap);
-	else if (**format == '%')
+	else if (flags.percent == 1)
 		len += ft_putchar('%');
-	// или так - или выводить не по символьно а по строкам (может объединить это условие и условие выше?)
-	else
-		len += ft_putchar('%');
-	//чтобы не зайти за строку если % последний символ
+
 	if (**format != '\0')
 		(*format)++;
 	return (len);
@@ -45,9 +44,7 @@ int		ft_print_all(va_list ap, const char *format)
 
 	len = 0;
 	while (*format)
-		if ((*format == '%')) // && следуюий символ не конец строки
-			// if ((ft_parser(ap, &format)) < 0)
-			// 	return (-1); ???
+		if ((*format == '%'))
 			len += ft_handler_ap(ap, &format);
 		else
 			len += ft_putchar(*(format++));
@@ -76,13 +73,13 @@ int		main()
 
 	// ft_printf
 	// printf("FT_PRINTF: ");
-    i = ft_printf(test_str, 123);
+    i = ft_printf(test_str, 3451);
     // printf(" | RETURN: %d", i);
 	printf("\n");
 
 	// printf
 	// printf("PRINTF   : ");
-	j = printf(test_str, 123);
+	j = printf(test_str, 3451);
     // printf(" | RETURN: %d", j);
 	printf("\n");
     return (0);
