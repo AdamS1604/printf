6a531c4bca76dc2e6fbc32e6b36da47a5d532367
Adam <adamchehab@gmail.com>
2021-02-10 15:50:38 +0300

    Added: *width parse

--- main.c
+++ main.c
@@ -27,26 +27,33 @@ int		ft_flags_parser(const char **format, t_spec **spec)
 	return (len);
 }
 
-int		ft_width_parser(const char **format, t_spec **spec)
+int		ft_width_parser(va_list ap, const char **format, t_spec **spec)
 {
 	int len;
 	int num;
 
-	num = ft_atoi(*format);
-	if (num != 0)
+	len = 0;
+
+	// TODO: Optimise this code
+	if (**format == '*')
+	{
+		(*spec)->width = va_arg(ap, int);
+		len += 1;
+	}
+	else if ((num = ft_atoi(*format)) != 0) // Можно в width просто класть результат атои сразу
 		(*spec)->width = num;
-	len = ft_get_nbr_len(num, 10);
+	len += ft_get_nbr_len(num, 10);
 	(*format) += len;
 	return (len);
 }
 
-int		ft_parser(const char *format, t_spec *spec)
+int		ft_parser(va_list ap, const char *format, t_spec *spec)
 {
 	int len;
 	
 	ft_init_spec(&spec);
 	len = ft_flags_parser(&format, &spec);
-	len += ft_width_parser(&format, &spec);
+	len += ft_width_parser(ap, &format, &spec);
 	// parse accuracy
 	if (ft_strchr("diucspxX%", *format))
 		spec->type = *format;
@@ -74,7 +81,7 @@ int		ft_handler_ap(va_list ap, const char **format)
 	t_spec spec;
 
 	len = 0;
-	parse = ft_parser(++(*format), &spec);
+	parse = ft_parser(ap, ++(*format), &spec);
 
 	if (parse == 0)
 		return (-1);
@@ -125,12 +132,12 @@ void ft_test(char *str)
 	int j = 0;
 
 	// my
-	i = ft_printf(str, 556);
+	i = ft_printf(str, 10, 5);
 	printf(" | RETURN: %d", i);
 	printf("\n");
 
 	// standart
-	j = printf(str, 556);
+	j = printf(str, 10, 5);
 	printf(" | RETURN: %d", j);
 	printf("\n");
 
@@ -139,16 +146,17 @@ void ft_test(char *str)
 
 int		main(void)
 {
-	ft_test("%");
-	ft_test("%%");
-	ft_test("%%%");
-	ft_test("%d");
-	ft_test("%z");
-	ft_test("%z 123");
-	ft_test("test %d");
-	ft_test("% d 123");
-	ft_test("%       d 123");
-	ft_test("%  0-  12d");
+	// ft_test("%");
+	// ft_test("%%");
+	// ft_test("%%%");
+	// ft_test("%d");
+	// ft_test("%z");
+	// ft_test("%z 123");
+	// ft_test("test %d");
+	// ft_test("% d 123");
+	// ft_test("%       d 123");
+	// ft_test("%  0-  12d");
+	ft_test("%*5d");
 
     return (0);
 }
