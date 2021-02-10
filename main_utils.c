3cbe783cf0ca2ac22fe61a5e3667e4a0d151e78b
Adam <adamchehab@gmail.com>
2021-02-10 15:49:42 +0300

    Added: width parser

--- main_utils.c
+++ main_utils.c
@@ -62,3 +62,33 @@ char    *ft_strchr(const char *s, int c)
         return ((char *)s);
     return (NULL);
 }
+
+int		ft_atoi(const char *str1)
+{
+	int				flag;
+	unsigned char	*str;
+	int				i;
+
+	str = (unsigned char *)str1;
+	i = 0;
+	flag = 0;
+	while (((*str == 32) || (*str == '\f') || (*str == '\n'))
+	|| ((*str == '\t') || (*str == '\v') || (*str == '\r')))
+	{
+		str++;
+	}
+	if (*str == 45)
+	{
+		flag = 1;
+		str++;
+	}
+	else if (*str == 43)
+		str++;
+	while ((*str >= 48) && (*str <= 57))
+	{
+		i = i * 10 + (*str - 48);
+		str++;
+	}
+	return (flag == 0 ? i : -i);
+}
+
