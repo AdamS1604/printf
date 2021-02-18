7ae290a77157d67a74c9b4c5d6e0e3472fb87e17
Adam <adamchehab@gmail.com>
2021-02-18 12:02:03 +0300

    New handler_ap - optimised code

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -3,30 +3,27 @@
 int		ft_handler_ap(va_list ap, const char **format)
 {
 	int len;
-	int parse;
-	t_spec spec;
+    int parse;
+    t_spec spec;
 
-	parse = ft_parser(ap, ++(*format), &spec);
+	// parse flags (++ cuz skip %)
+    parse = ft_parser(ap, ++(*format), &spec);
 
-	// invalid flag and nothing more to output
-	if (parse == 0)
-		return (-1);
-	// output invalid foramt
-	else if (parse > 0)
-		len = ft_format_out(format, spec);
-	// output ap
-	else if ((parse *= -1) > 0)
-		// TODO should return -1 if malloc error
-		len = ft_handler_hub(ap, spec); 
-	
-    // ? fix there
-	// if (parse < 0)
-	// 	parse *= -1;
-	// else if (parse > 0)
-	// 	return (len);
-	(*format) += parse;
-	// mallocs defence
-	return ((len < 0) ? -1 : len);
+    // invalid flag and nothing more to output
+    if (parse == 0)
+        return (-1);
+    // output invalid foramt
+    else if (parse > 0)
+        len = ft_format_out(format, spec);
+    // output ap
+    else if ((parse *= -1) > 0)
+		// TODO should return - 1 on error
+        len = ft_handler(ap, spec);
+
+	// increace foramt (move)
+    (*format) += parse;
+    // mallocs defence
+    return ((len < 0) ? -1 : len);
 }
 
 int		ft_print_all(va_list ap, const char *format)
